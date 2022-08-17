cfa7c71eb24d8a7c04efbff00b40cd2cab5cf357
VinsuLaw <vinsentlou9@gmail.com>
2022-08-17 15:42:37 +0300

    main components

--- app/src/components/main/header-nav.component.vue
+++ app/src/components/main/header-nav.component.vue
@@ -1,5 +1,36 @@
 <template>
-    <h3>HeaderNavComponent</h3>
+    <div class="header-nav container">
+        <router-link to="/">
+            <img src="../../assets/store.png" class="header-nav_logo" />
+        </router-link>
+        <ul class="header-nav__navigation">
+            <li><router-link to="/news">News</router-link></li>
+            <li>
+                <router-link to="/payment">Payment and delivery</router-link>
+            </li>
+            <li>
+                <router-link to="/refund">Refund and warranty</router-link>
+            </li>
+            <li><router-link to="/faq">FAQ</router-link></li>
+            <li><router-link to="/contacts">Contacts</router-link></li>
+            <li><router-link to="/about">About us</router-link></li>
+        </ul>
+        <div class="header-nav__socials">
+            <a href="#">
+                <img src="../../assets/vk-icon.svg" alt="" />
+            </a>
+            <a href="#">
+                <img src="../../assets/telegram-icon.svg" alt="" />
+            </a>
+            <a href="#">
+                <img src="../../assets/tiktok-icon.png" alt="" />
+            </a>
+        </div>
+        <div class="header-nav__calling">
+            <span>Calls 9:00-18:00 (TKY)</span>
+            <h3>+1 (111) 000-00-00</h3>
+        </div>
+    </div>
 </template>
 
 <script>
--- app/src/components/main/header.component.vue
+++ app/src/components/main/header.component.vue
@@ -1,5 +1,39 @@
 <template>
-    <h3>Header</h3>
+    <header class="header">
+        <div class="header__container container">
+            <ul class="header__nav">
+                <li>
+                    <router-link to="/manga">Manga</router-link>
+                </li>
+                <li>
+                    <router-link to="/ranobe">Ranobe</router-link>
+                </li>
+                <li>
+                    <router-link to="/figures">Anime figures</router-link>
+                </li>
+            </ul>
+            <div class="header__search_container">
+                <input type="text" class="header__search" />
+                <span class="material-icons">search</span>
+            </div>
+
+            <div class="header__buttons">
+                <div class="header__buttons_container">
+                    <router-link to="/cart" class="header_btn">
+                        <span class="material-icons">shopping_cart</span>
+                    </router-link>
+                    <router-link to="/favorites" class="header_btn">
+                        <span class="material-icons">favorite_border</span>
+                    </router-link>
+                </div>
+
+                <div class="header__authorization-btns">
+                    <router-link to="/registration">Registration</router-link>
+                    <router-link to="/login">Login</router-link>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
